1f40dd493d4bef8aee7e08f09b014c7256458075
Tirth Vanparia <98956837+T1eerth@users.noreply.github.com>
2022-04-18 10:18:54 +0530

    Add files via upload

---
+++ cpp basics/dynamicdeclaration.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(int i=0;i<10;i++)
+    {
+        
+    }
+    cout<<i;
+    return 0;
+
+}
+    /*therefore the the int=i will be stored only for that for loop and further down it removes the int 
+from it;s memory
