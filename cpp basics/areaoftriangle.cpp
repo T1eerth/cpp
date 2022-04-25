c41aa312a67405ecab4a7f70868c236b6cafa7ad
Tirth Vanparia <tirth.vanparia16@gmail.com>
2022-04-25 09:03:24 +0530

    Add files via upload

---
+++ cpp basics/areaoftriangle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float a,b,c;
+    cout<<"enter the height and the base";
+    cin>>a>>b;
+    c=(a*b)/2;
+    cout<<"the area is"<<c;
+    return 0;
+
+}
