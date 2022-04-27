59d49af48695395cf95e6cd87bbfd693e7813b1a
Tirth Vanparia <tirth.vanparia16@gmail.com>
2022-04-28 05:22:01 +0530

    Add files via upload

---
+++ Loops/reverseofano.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,r,rev=0;
+    cout<<"Enter the number";
+    cin>>n;
+    while(n>0)
+    {r=n%10;
+    n=n/10;
+    rev=rev*10+r;
+    cout<<rev;}
+    return 0;
+
+}
+
