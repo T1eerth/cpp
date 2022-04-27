241ed4dec488f0eeacb90714797795bd6949c0d1
Tirth Vanparia <tirth.vanparia16@gmail.com>
2022-04-27 21:27:28 +0530

    Add files via upload

---
+++ Loops/dowhile.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i=1;
+    cout<<"Enter the range";
+    cin>>n;
+    do
+    {
+        cout<<i;
+        i++;}
+        while(i<=n);
+    
+    return 0;
+}
